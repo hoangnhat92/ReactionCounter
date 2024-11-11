20e281c4952af568bc81854985d63fb483157108
nhatdoan1992 <nhat@coderpush.com>
2024-11-11 20:08:15 +0700

    Initial Commit

---
+++ ReactionCounter/ReactionCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionCounterApp.swift
+//  ReactionCounter
+//
+//  Created by nhat on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ReactionCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

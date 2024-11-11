56286b55cb3f73892bff221befa564cbc9cf7b85
nhatdoan1992 <nhat@coderpush.com>
2024-11-11 20:35:28 +0700

    Initial setup

---
+++ ReactionCounter/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ReactionCounter
+//
+//  Created by nhat on 11/11/24.
+//
+
+import UIKit
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+#if DEBUG
+Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection.bundle")?.load()
+//for tvOS:
+Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/tvOSInjection.bundle")?.load()
+//Or for macOS:
+Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/macOSInjection.bundle")?.load()
+#endif
+        
+        return true
+    }
+}
--- ReactionCounter/ContentView.swift
+++ ReactionCounter/ContentView.swift
@@ -6,16 +6,62 @@
 //
 
 import SwiftUI
+import Inject
 
 struct ContentView: View {
+    
+    @State private var thumbsUpCount = 0
+    @State private var heartCount = 0
+    @State private var laughCount = 0
+    
+    @ObserveInjection var inject
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Text("React to this post!")
+               .font(.title)
+               .padding(.bottom, 20)
+           
+           HStack(spacing: 40) {
+               ReactionButton(emoji: "👍", count: $thumbsUpCount)
+               ReactionButton(emoji: "❤️", count: $heartCount)
+               ReactionButton(emoji: "😂", count: $laughCount)               
+           }
+           .padding()           
+           Spacer()
+       }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.green)        
+        .injectEnabled()
+    }
+}
+
+struct ReactionButton: View {
+    let emoji: String
+    @Binding var count: Int
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                count += 1
+            }) {
+                Text(emoji)
+                    .font(.largeTitle)
+            }
+            
+            Text("\(count)")
+                .font(.headline)
         }
-        .padding()
+    }
+}
+
+extension View {
+    func injectEnabled() -> some View {
+        #if DEBUG
+        return self.enableInjection()
+        #else
+        return self
+        #endif
     }
 }
 
